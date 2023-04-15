8d723d415c71770b82325106551f2cfd2fe9312d
ronak-d <ronakdodeja73@gamil.com>
2023-04-15 20:14:52 +0530

    color badges made dynamically using js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 let colors = ["blue", "pink", "yellow"];
 
-let run = document.getElementById("leftBox");
-run.style.color = "blue";
+let colorDiv = document.getElementById("colorBadge");
+
+for (let i = 0; i < colors.length; i++) {
+  // Creates a new div element
+  let newDiv = document.createElement("div");
+
+  newDiv.style.backgroundColor = colors[i];
+  newDiv.style.width = "25px";
+  newDiv.style.height = "25px";
+  newDiv.style.borderRadius = "50%";
+  newDiv.style.marginRight = "10px";
+  //   newDiv.style.border = "2px solid black";
+
+  colorDiv.appendChild(newDiv);
+}
